e50b44ecb68c52883efc0969afaba70b6ce51a1f
QueroXD <alvaroqueroariza@gmail.com>
2024-10-16 18:54:15 +0200

    Clear code
    Remove unecesary code

--- src/app/modelsdedades/juegos.ts
+++ src/app/modelsdedades/juegos.ts
@@ -3,4 +3,4 @@ export class juegos {
     released: string | undefined;
     background_image: string | undefined;
     metacritic: string | undefined;
-    }
+}
--- src/app/modelsdedades/resultatqry.ts
+++ src/app/modelsdedades/resultatqry.ts
@@ -1,8 +1,4 @@
 import { juegos } from './juegos';
 export class Resultatqry {
-    count: number = 0;              // Número total de resultados
-    next: string | null = null;     // URL para la siguiente página de resultados
-    previous: string | null = null; // URL para la página anterior de resultados
-    results: juegos[] = [];         // Array de objetos de tipo Result (que contiene los juegos)
-    user_platforms: boolean = false;// Información sobre las plataformas del usuario (según tu JSON)
+    results: juegos[] = [];       
 }
